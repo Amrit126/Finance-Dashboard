1db4f551d090427ad28e5a34d795528c7a3d747c
Amrit Paudel <amritpoudel0100@gmail.com>
2025-12-11 19:37:52 +0545

    Creating filtered data function to filter data as the need of user

---
+++ src/utils/getFilteredData.js
@@ -0,0 +1,25 @@
+export function getFilteredData(transactions, type, category, sortConfig) {
+    let tempData = [...transactions]
+
+    if (type) {
+        tempData = tempData.filter(data => data.type === type)
+    }
+
+    if (category) {
+        tempData = tempData.filter(data => data.category === category)
+    }
+
+    if (sortConfig) {
+        if (sortConfig === "Amount↑") {
+            tempData = tempData.sort((a, b) => a.amount - b.amount)
+        } else if (sortConfig === "Amount↓") {
+            tempData = tempData.sort((a, b) => b.amount - a.amount)
+        } else if (sortConfig === "Date↑") {
+            tempData = tempData.sort((a, b) => new Date(a.date) - new Date(b.date))
+        } else {
+            tempData = tempData.sort((a, b) => new Date(b.date) - new Date(a.date))
+        }
+    }
+
+    return tempData
+}
