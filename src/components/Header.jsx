70f5878788ce0b7d3f37e93879de3e3357aee3bd
Amrit Paudel <amritpoudel0100@gmail.com>
2025-12-12 18:37:46 +0545

    Making header component responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,8 +14,8 @@ export function Header() {
 
     return (
         <>
-            <div className="flex justify-between items-center p-4 bg-slate-900 text-white text-4xl border-b-2 
-            border-black z-50 relative">
+            <div className="flex justify-between items-center p-4 bg-slate-900 text-white  text-2xl md:text-4xl 
+            border-b-2 border-black z-50 relative">
                 <Link className="mx-auto md:mx-0 font-extrabold tracking-wide" to="/">FinSight</Link>
                 <div className="hidden md:flex gap-12 mx-auto text-3xl">
                     <LinkElements />
@@ -27,8 +27,8 @@ export function Header() {
                 </div>
             </div >
             <div className={`${showMenu ? 'translate-y-0' : '-translate-y-full'} 
-            flex justify-between md:hidden text-2xl px-4 py-2 transition-all duration-300 absolute bg-white w-full
-            border-black border-b-2`}>
+            flex justify-between md:hidden text-lg md:text-2xl px-4 py-2 transition-all duration-300 absolute bg-white w-full
+            border-black border-b`}>
                 <LinkElements />
             </div>
         </>
