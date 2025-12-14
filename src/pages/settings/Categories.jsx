95ea5811423aed542d4a8641d3938502e07b7831
Amrit Paudel <amritpoudel0100@gmail.com>
2025-12-14 11:21:39 +0545

    Creating categories list in settings page and adding new one

---
+++ src/pages/settings/Categories.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+export function Categories({ categories, setCategories }) {
+    const [isAdding, setIsAdding] = useState(false)
+    const [inputText, setInputText] = useState('')
+
+    function toggleIsAdding() {
+        setIsAdding(true)
+    }
+
+    function cancelAdding() {
+        setInputText('')
+        setIsAdding(false)
+    }
+
+    function changeInputText(e) {
+        setInputText(e.target.value)
+    }
+
+    function submitNewCategory() {
+        if (categories.includes(inputText)) {
+            alert("Category already available")
+            return
+        }
+        const temp = [...categories, inputText]
+        setCategories(temp)
+        cancelAdding()
+    }
+
+    return (
+        <div>
+            <h2 className="font-extrabold text-2xl md:text-3xl my-4">Categories</h2>
+            <div className="grid grid-cols-2 text-sm md:text-xl md:grid-cols-3 gap-3">
+                {categories.map(category => (
+                    <div key={category} className="bg-white flex justify-between items-center p-3">
+                        <p>{category}</p>
+                        <button className="cursor-pointer ">X</button>
+                    </div>
+                ))}
+            </div>
+
+            <div className="flex justify-center mt-4">
+                <div className={`${isAdding ? 'hidden' : ''}`}>
+                    <button className="bg-slate-700 p-2 text-white cursor-pointer"
+                        onClick={toggleIsAdding}
+                    >+</button>
+                </div>
+                <div className={`${isAdding ? 'flex' : 'hidden'} flex-col items-center p-2
+                            bg-gray-300 rounded-xl`}>
+                    <h3 className="font-bold">New Category</h3>
+                    <label className="mt-4">Name:
+                        <input className="bg-white p-1 mx-2 text-black focus:outline-none
+                                    focus:ring-2 focus:ring-blue-600 rounded-lg mx-"
+                            type="text"
+                            placeholder="New Category..."
+                            value={inputText}
+                            onChange={changeInputText}
+                        />
+                    </label>
+                    <div className="flex gap-2 mt-4">
+                        <button className="bg-red-500 text-black rounded-xl p-2
+                                    cursor-pointer transition-all duration-300 hover:opacity-80"
+                            onClick={cancelAdding}
+                        >Cancel</button>
+                        <button className="bg-blue-500 text-black rounded-xl p-2
+                                    cursor-pointer transition-all duration-300 hover:opacity-80"
+                            onClick={submitNewCategory}
+                        >Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,8 +1,8 @@
 //import { useState } from 'react'
 import { Header } from "../../components/Header";
+import { Categories } from "./Categories";
 
-export function Settings({ isDark, setIsDark }) {
-
+export function Settings({ isDark, setIsDark, categories, setCategories }) {
     function toggleButton() {
         setIsDark(isOn => {
             return isOn ? false : true
@@ -13,22 +13,26 @@ export function Settings({ isDark, setIsDark }) {
         <>
             <title>Settings</title>
             <Header />
-            <div className="h-screen bg-slate-100 flex flex-col gap-4 px-8 py-6 text-xl
-            text-slate-700 font-bold">
-                {/*Dark mode toggle */}
-                <h2 className="text-3xl font-extrabold">Dark Mode Toggle</h2>
-                <div className="flex p-2 border border-black gap-8 w-full md:w-[30%] justify-between items-center">
-                    <p>Dark Mode</p>
-                    <div className={`h-[30px] w-[20%] relative rounded-lg
+            <div className="h-full bg-slate-100 px-4 md:px-8 py-6 text-xl
+            text-slate-700 font-bold flex flex-col items-center gap-4 rounded-xl">
+                <h1 className="font-extrabold text-4xl md:text-6xl">Settings</h1>
+                <div className="border border-black w-full md:w-1/2 p-4 ">
+                    <div className="flex justify-between">
+                        <p>Dark Mode</p>
+                        <div className={`h-[30px] w-[60px] relative rounded-lg
                     ${isDark ? 'bg-black' : 'bg-slate-300'} cursor-pointer`}
-                        onClick={toggleButton}
-                    >
-                        <div className={`absolute h-[30px] w-[50%] ${isDark ? 'bg-white' : 'bg-black'} 
+                            onClick={toggleButton}
+                        >
+                            <div className={`absolute h-[30px] w-[30px] ${isDark ? 'bg-white' : 'bg-black'} 
                         rounded-3xl ${isDark ? 'translate-x-full' : 'translate-x-0'} 
                          transition-all duration-300`}>
 
+                            </div>
                         </div>
                     </div>
+
+                    {/*For Categories*/}
+                    <Categories categories={categories} setCategories={setCategories} />
                 </div>
             </div>
         </>
